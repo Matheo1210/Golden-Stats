40e4081e46e5958e3903706f60299a985454e1e7
Matheo Hanss <hanssmatheo@gmail.com>
2025-02-10 14:06:50 +0100

    refactor: improve player tile creation and update player profile page structure

--- src/profile.js
+++ src/profile.js
@@ -1,16 +1,16 @@
 import { BalldontlieAPI } from "@balldontlie/sdk";
 
-const api = new BalldontlieAPI({ apiKey: "e36f3fea-f428-482f-a986-bad13615e506" });
-
-
+const api = new BalldontlieAPI({
+  apiKey: "e36f3fea-f428-482f-a986-bad13615e506",
+});
 
 function createPlayerTile(player) {
-    return `
+  return `
     <article class="tile">
       <div class="tile-header">
         <h3><span>${player.name}</span><span>${player.team}</span></h3>
       </div>
-      <a href="#">
+      <a href="/info-player.html">
         <span>View Profile</span>
         <span class="icon-button">
           <i class="ph-caret-right-bold"></i>
@@ -20,36 +20,37 @@ function createPlayerTile(player) {
     `;
 }
 
-
 console.log(playerSearch.value);
-api.nba.getPlayers({ per_page: 5, search: playerSearch.value })
-    .then(players => {
-        const tilesContainer = document.getElementById('player-tiles');
-        players.data.forEach(player => {
-            tilesContainer.innerHTML += createPlayerTile({
-                name: `${player.first_name} ${player.last_name}`,
-                team: player.team.full_name
-            });
-        });
-    })
-    .catch(error => console.error('Error fetching player data:', error));
+api.nba
+  .getPlayers({ per_page: 5, search: playerSearch.value })
+  .then((players) => {
+    const tilesContainer = document.getElementById("player-tiles");
+    players.data.forEach((player) => {
+      tilesContainer.innerHTML += createPlayerTile({
+        name: `${player.first_name} ${player.last_name}`,
+        team: player.team.full_name,
+      });
+    });
+  })
+  .catch((error) => console.error("Error fetching player data:", error));
 
 function SearchPlayer() {
-    const playerSearch = document.getElementById('playerSearch');
-    console.log(playerSearch.value);
-    api.nba.getPlayers({ per_page: 5, search: playerSearch.value })
-        .then(players => {
-            const tilesContainer = document.getElementById('player-tiles');
-            tilesContainer.innerHTML = '';
-            players.data.forEach(player => {
-                tilesContainer.innerHTML += createPlayerTile({
-                    name: `${player.first_name} ${player.last_name}`,
-                    team: player.team.full_name
-                });
-            });
-        })
-        .catch(error => console.error('Error fetching player data:', error));
+  const playerSearch = document.getElementById("playerSearch");
+  console.log(playerSearch.value);
+  api.nba
+    .getPlayers({ per_page: 5, search: playerSearch.value })
+    .then((players) => {
+      const tilesContainer = document.getElementById("player-tiles");
+      tilesContainer.innerHTML = "";
+      players.data.forEach((player) => {
+        tilesContainer.innerHTML += createPlayerTile({
+          name: `${player.first_name} ${player.last_name}`,
+          team: player.team.full_name,
+        });
+      });
+    })
+    .catch((error) => console.error("Error fetching player data:", error));
 }
 
-const searchButton = document.getElementById('player-search-button');
-searchButton.addEventListener('click', SearchPlayer);
+const searchButton = document.getElementById("player-search-button");
+searchButton.addEventListener("click", SearchPlayer);
